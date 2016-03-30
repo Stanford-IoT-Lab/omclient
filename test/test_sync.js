583fba2de0a44d2faad3926013e3560e1f9d3a08
Ben Dodson <bjdodson@gmail.com>
2016-03-30 13:33:29 -0700

    persistence may be disabled from omlib config

--- test/test_sync.js
+++ test/test_sync.js
@@ -2,5 +2,6 @@ var assert = require('assert')
 var omlib = (typeof window === 'undefined') ? require('../lib/omlib') : require('omlib');
 
 omlib.init({
-    sync: true
+    sync: true,
+    persist: false,
 });
