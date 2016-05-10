3c267c1a80ae6a17b310ef19592273c841724da7
Ben Dodson <bjdodson@gmail.com>
2016-05-10 13:16:30 -0700

    remove old make-browserify.js script (use gulp browserify)

--- make-browserified.js
+++
@@ -1,7 +0,0 @@
-var browserify = require('browserify')
-, fs = require('fs');
-
-var b = browserify();
-
-b.require('./lib/omlib.js', { expose: 'omlib' });
-b.bundle().pipe(fs.createWriteStream(__dirname + '/build/omlib.js'));
